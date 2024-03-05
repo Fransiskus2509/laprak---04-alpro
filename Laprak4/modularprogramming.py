be215df67bf41d1191eea4c83ab38798175cd596
Fransiskus2509 <143708148+Fransiskus2509@users.noreply.github.com>
2024-03-05 20:09:44 +0700

    Add files via upload

---
+++ Laprak4/modularprogramming.py
@@ -0,0 +1,26 @@
+# umur = input("masukkan umurmu: ")
+# print("hallo umur saya adalah",umur,"tahun")
+
+# def tambah(a,b):
+#     hasil = a+b
+#     return hasil
+
+# a=tambah(10,5)
+# print("hasilnya adalah:",a)
+
+# def tambah(a,b):
+#     hasil = a+b
+#     return hasil
+
+# a = int(input("masukkan bilangan pertama: "))
+# b = int(input("masukkan bilangan ke dua: "))
+# print("ini adalah hasil dari bilangan pertama, dan dua:",tambah(a,b))
+
+# def hitung(belanja, diskon=0):
+#     hasil = belanja-(belanja*diskon)/100
+#     return hasil
+
+# print(hitung(50000))
+
+a = lambda a,b:a+b
+print(a(10,30))
